ccdab7bf361a624ae27bd6800f407bc66bfb1c26
dagimal <daffagifariakmal@gmail.com>
2022-05-07 00:44:03 +0700

    cobaKedua

--- link.py
+++ link.py
@@ -3,6 +3,8 @@ import sys
 import re
 from bs4 import BeautifulSoup
 
+OUTPUT_FILENAME = 'cf.txt'
+
 scraper = cloudscraper.create_scraper()
 url = scraper.get('https://www.creativefabrica.com/product/3d-festive-lantern-svg-cut-file-set/')
 soup = BeautifulSoup(url.text, 'html.parser')
@@ -14,7 +16,8 @@ def link_url():
 		link_loop = link.get('href')
 		regexp = re.compile(r'/product/')
 		if regexp.search(link_loop):
-			print(link_loop)
+			f = open(OUTPUT_FILENAME, 'a')
+			print(link_loop, file=f)
 
 
 link_url()
