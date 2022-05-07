d1d165d36abcb2957c9c07f73b9f8bea726ec496
dagimal <daffagifariakmal@gmail.com>
2022-05-07 18:28:04 +0700

    ujicoba

--- link.py
+++ link.py
@@ -3,21 +3,28 @@ import sys
 import re
 from bs4 import BeautifulSoup
 
-OUTPUT_FILENAME = 'cf.txt'
+OUTPUT_FILENAME = 'cf_link.txt'
 
 scraper = cloudscraper.create_scraper()
-url = scraper.get('https://www.creativefabrica.com/product/3d-festive-lantern-svg-cut-file-set/')
-soup = BeautifulSoup(url.text, 'html.parser')
 
-urls = []
+# Add Page URL File
+#page_link = open('page-url.txt', 'r').readlines()
+
+URL_PATTERN = ''
 
 def link_url():
-	for link in soup.find_all('a'):
-		link_loop = link.get('href')
-		regexp = re.compile(r'/product/')
-		if regexp.search(link_loop):
-			f = open(OUTPUT_FILENAME, 'a')
-			print(link_loop, file=f)
+	for line in range(1405):
+	#for line in page_link: # Use page_link variables
+		#url = scraper.get(line) # Use page_link variables
+		url = scraper.get(f'https://www.creativefabrica.com/subscriptions/fonts/script-handwritten/page/{line+1}')
+		soup = BeautifulSoup(url.text, 'html.parser')
 
+		for link in soup.find_all('a'):
+			link_loop = link.get('href')
+			regexp = re.compile(r'/product/')
+			if regexp.search(link_loop):
+				f = open(OUTPUT_FILENAME, 'a')
+				print(link_loop, file=f)
 
+# FunctionCall
 link_url()
